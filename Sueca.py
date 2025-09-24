f07368d4cb6db15d720f679a7e5d93e4adb1dccc
Daniel Andrade Martins <dan.andrade@ua.pt>
2025-09-24 16:30:41 +0100

    Commit 6 - Game is fully functional

--- Sueca.py
+++ Sueca.py
@@ -23,7 +23,7 @@ ranks_map = {
 
 suits = ["♥️","♦️","♣️","♠️"]
 
-MAX_CARDS = 40      # Probably not needed
+# MAX_CARDS = 40      # Probably not needed
 
 class Card:
     def __init__(self,rank,suit):
@@ -32,12 +32,6 @@ class Card:
 
     def __str__(self):
         return f"{self.rank}|{self.suit}"
-    
-class NullCard(Card):
-    def __init__(self, rank, suit):
-        super().__init__(rank, suit)
-    def __str__(self):
-        return f"Null|Card"
     
 class Deck:
     def __init__(self):
@@ -55,7 +49,9 @@ class Game:
     def __init__(self,player_names):
         self.deck = Deck()
         self._cut_deck()
+        print(f"Shuffling deck...")
         self._shuffle_deck()
+        print(f"Deck has been shuffled.")
         self.trump_card = self._get_trump_card()
         self.trump_suit = self.trump_card.suit
         self.players = [Player(name, i + 1) for i, name in enumerate(player_names)]
@@ -67,6 +63,9 @@ class Game:
                                 self.players[3]:0}
         self.round_counter = 1
         self.teams = [[self.players[0],self.players[2]],[self.players[1],self.players[3]]]
+        self.last_round_winner = self.players[0]
+        self.round_suit = ''
+        self.current_player = self.last_round_winner
     # def __str__(self):
     #     players_str = ''
     #     for i in range(0,len(self.players),2):
@@ -76,11 +75,16 @@ class Game:
     #         f"Players =>\n{players_str}\n"
     #     )
     def _get_trump_card(self):
-        answer = input("Take trump_card from top or bottom?: ").strip().lower()
-        if answer=="top":
-            trump_card = self.deck.pile[0]
-        else:
-            trump_card = self.deck.pile[-1]
+        while True:
+            answer = input("Take trump_card from top or bottom?: ").strip().lower()
+            if answer=="top":
+                trump_card = self.deck.pile[0]
+                break
+            elif answer == "bottom":
+                trump_card = self.deck.pile[-1]
+                break
+            else:
+                print(f"Invalid answer, try again. ")
         self.trump_card = trump_card
         return trump_card
 
@@ -90,15 +94,19 @@ class Game:
 
     def _cut_deck(self):
         print("Deck before the cut:")
-        print(self._show_deck())
+        print(self.get_deck())
 
-        index = int(input("Cut from what index:").strip())
-        top = self.deck.pile[:index]
-        bottom = self.deck.pile[index:]
+        while True:
+            index = int(input("Cut from what index:").strip())
+            if 0 < index <= 40:
+                top = self.deck.pile[:index]
+                bottom = self.deck.pile[index:]
+                break
+            print(f"Invalid cut index, pick in a range of (1,40)")
         self.deck.pile = bottom + top
 
         print("Deck after the cut:")
-        print(self._show_deck())
+        print(self.get_deck())
 
     def _distribute_cards(self):     # Can only be done after shuffling needs to be 10 to right etc
         for player in self.players:
@@ -107,20 +115,52 @@ class Game:
                 card = self.deck.pile.pop(0)  
                 set_cards.append(card)
             player.receive_cards(set_cards)
+
+    def assure_it_can_be_played(self, card, player):
+        has_round_suit = any(c.suit == self.round_suit for c in player.hand)
+        if card.suit == self.round_suit:
+            return True
+        if card.suit == self.trump_card.suit:
+            return True
+        if has_round_suit:
+            return False
+       
+        return True
+
     
     def _round(self):
         print(f"========================================")
         print(f"Round {self.round_counter}")
         round_vector = []
-        for player in self.players:
-            print(f"It's Player {player.player_id}'s turn:")
+        current_player = self.last_round_winner
+        start_index = self.players.index(self.last_round_winner)
+        turn_order = self.players[start_index:] + self.players[:start_index]
+        for player in turn_order:
+            print(f"It's Player {player.player_id}'s turn: ({player.name})")
             print(player._view_hand())
-            round_vector.append(player.play_card())
+            if player == self.last_round_winner:
+                print(f"Player {player.player_id} takes the lead this round, as they played the strongest last round, or distributed the deck.")
+                round_vector.append(player.play_card())
+                self.round_suit = round_vector[0].suit
+                print(f"This round's suit is {self.round_suit}. You're forced to play a card of suit {self.round_suit} if you have one in hand! You can play a card with the trump suit {self.trump_card.suit}  alternatively.")
+            else:
+                while True:
+                    possible_card = player.play_card()
+                    if self.assure_it_can_be_played(possible_card, player):
+                        round_vector.append(possible_card)
+                        break
+                    else:
+                        print(f"You must follow suit ({self.round_suit})!")
+                        player.hand.append(possible_card)
+
         print("\nCards played this round:")
         for i, card in enumerate(round_vector, start=1):
             print(f"Player {self.players[i-1].player_id} played: {card}")
 
-        self.round_vector_history.append(round_vector)
+        self.round_vector_history.append({
+            "round_suit": self.round_suit,
+            "cards": round_vector[:]
+        })
 
         def _determine_round_winner():
             trump_was_played = any(card.suit == self.trump_suit for card in round_vector)
@@ -137,7 +177,8 @@ class Game:
             return winner,winner_index
 
         round_winner, winner_index = _determine_round_winner()
-        winner_player = self.players[winner_index]  # map index to player
+        winner_player = self.players[winner_index]
+        self.last_round_winner = winner_player
 
         def _get_round_sum():
             round_sum = 0
@@ -150,27 +191,48 @@ class Game:
         # Update score
         self.players_and_scores[winner_player] += _get_round_sum()
 
+        while True:
+            answer = input("View game history so far?").strip().lower()
+            if answer=="yes":
+                print("Game history vector:")
+                print(self.get_history())
+                break
+            break
         self.round_counter+=1
 
-    def _show_deck(self):
+    def get_deck(self):
         return str(self.deck)
 
-    def _show_players(self):
+    def get_players (self):
         return str(self.players)
     
-    def _show_trump_card(self):
+    def get_trump_card(self):
         return str(self.trump_card)
 
-    def _show_trump_suit(self):
+    def get_trump_suit(self):
         return str(self.trump_suit)
     
-    def _show_players_and_scores(self):
+    def get_players_and_scores(self):
         return str(self.players_and_scores)
     
-    def _show_history(self):
-        return str(self.round_vector_history)
+    def get_history(self):
+        history_str = ""
+        for round_num, round_data in enumerate(self.round_vector_history, start=1):
+            cards = round_data["cards"]
+            suit = round_data["round_suit"]
+
+            history_str += f"Round {round_num} (suit: {suit}):\n"
+            if not cards:
+                history_str += "  (no cards recorded)\n"
+            else:
+                for i, card in enumerate(cards):
+                    player = self.players[i]  # careful: assumes fixed order
+                    history_str += f"  Player {player.player_id} ({player.name}) played {card}\n"
+            history_str += "\n"
+
+        return history_str
     
-    def _show_teams(self):
+    def get_teams(self):
         return str(self.teams)
     
     def _show_final_scores_and_print_winner(self):
@@ -197,31 +259,29 @@ class Player:
     def play_card(self):
         while True:
             try:
-                print("Select a card by its number (1 to {}):".format(len(self.hand)))
+                print(f"Select a card by its number (1 to {len(self.hand)}):")
                 choice = int(input("Choice? ").strip()) - 1  
                 if 0 <= choice < len(self.hand):
-                    return self.hand.pop(choice)
+                    card = self.hand.pop(choice)
+                    return card
                 else:
                     print("Invalid choice, try again.")
             except ValueError:
                 print("Please enter a valid number.")
 
 
-    def _play_cut(self):
-        """Can cut plays made by other players might not be necessary, look above fix later"""
-
     def __repr__(self):
         return f"Player {self.player_id} == {self.name}"
     
     def _view_hand(self):
-        print(f"Hand of Player {self.player_id} =>")
-        hand_str = ' '.join(str(card) for card in self.hand)  
+        print(f"Hand of Player {self.player_id} ({self.name})=>")
+        hand_str = '    '.join(str(card) for card in self.hand)  
         return hand_str
 
 def main():
     game = Game(["Pedro","Tiago","Lucas","Gonçalo"])
     i = 0
-    print(game._show_trump_card())
+    print(f"This game's trump is {game.get_trump_card()}")
     while i<10:
         game._round()
         i+=1      
